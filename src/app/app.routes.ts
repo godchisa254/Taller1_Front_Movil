ed4a671f980a6eeeabb443daaf9e0d30489c3e5e
godchisa254 <Matias.254@outlook.com>
2025-01-05 20:51:54 -0300

    feat: adds product redirecting aftrer logging in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,26 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './user/page/login/login.component';
+import { authGuard } from './user/guards/auth.guard';
+import { ListComponent } from './product/page/list/list.component';
+import { NotFoundComponent } from './shared/component/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    component: LoginComponent,
+  },
+  {
+    path: 'products',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'list',
+        component: ListComponent,
+      },
+    ],
+  },
+  {
+    path: '404',
+    component: NotFoundComponent,
   },
 ];
