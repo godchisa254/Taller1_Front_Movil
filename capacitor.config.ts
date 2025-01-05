2a96738f837b2df120605946645cb194c27ea4c6
godchisa254 <Matias.254@outlook.com>
2025-01-05 02:14:29 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Taller1_Front_Movil',
+  webDir: 'www'
+};
+
+export default config;
