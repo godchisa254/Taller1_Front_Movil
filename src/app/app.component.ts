324a13e0457d4894820561e897526a4f0dd00b7d
godchisa254 <Matias.254@outlook.com>
2025-01-05 23:19:32 -0300

    fix: adds tab bar into main template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone'; 
+import { TabbarComponent } from './shared/component/tabbar/tabbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, TabbarComponent],
 })
 export class AppComponent {
   constructor() {}
